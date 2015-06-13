30dc2b9a46456310e33f72fd6365de367adaa735
Ishikawa <ishiakwa@example.com>
2015-06-12 22:33:37 -0300

    solution problem A, round 307 div 2

---
+++ Round307Div2/A.py
@@ -0,0 +1,22 @@
+#http://codeforces.com/contest/551/problem/A
+
+#!/usr/bin/python
+
+import  re
+import sys
+from array import *
+
+n = int(raw_input())
+rating = []
+sol = []
+a = raw_input()
+rating = [int(p) for p in a.split(' ')]
+for x in range(0,n):
+    cont = 1
+    for y in range(0,n):
+        if rating[x] < rating[y]:
+            cont+= 1
+    sol.append(cont)
+
+for y in sol:
+    print y,
