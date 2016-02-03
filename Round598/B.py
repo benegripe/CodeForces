bda672a7562532ce81c10facf454cf5b0ce6f100
Benegripe <fabiolima39@gmail.com>
2016-02-03 00:29:38 -0200

    problem B

---
+++ Round598/B.py
@@ -0,0 +1,9 @@
+#http://codeforces.com/contest/598/problem/B
+#Solved by Benegripe
+#!/usr/bin/python
+
+s = raw_input()
+n = int(raw_input())
+for i in (0,n):
+    l,r,k = raw_input().split()
+    print l,r,k
